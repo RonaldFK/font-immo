dfa86626be69d4b7e6d25f2e52bdc1d79dcbdeb2
Ronald <ronaldfk.pro@gmail.com>
2023-02-11 16:37:19 +0100

    recupération de la liste des biens ok sans token pour l'instant

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <div>
           <router-link to="/">Home</router-link> |
           <router-link to="/login">Login</router-link> |
-          <router-link to="/estate">Les Biens</router-link> |
+          <router-link to="/estate" >Les Biens</router-link> |
           <router-link to="/test">Les Managers</router-link> |
           <router-link to="/test">Les Cliens</router-link> |
 
@@ -22,6 +22,32 @@ export default {
   components: {
     // FormAuth,
     
+  },
+  data(){
+    return{
+      baseUrl:'http://localhost:3000',
+    }
+  },
+  methods:{
+    async test(){
+      console.log('test');
+      try{
+            const response = await fetch(`${this.baseUrl}/estate`,
+            {
+                method:'GET',
+                headers:{
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin":"http://localhost:8080"
+                }
+            })
+            const result = await response.json()
+            // this.estates.push(response)
+            console.log(result);
+            
+        } catch(err){
+            console.log(err);
+        }
+    }
   }
 }
 
--- src/views/EstatetView.vue
+++ src/views/EstatetView.vue
@@ -1,11 +1,65 @@
 <template>
-    <!-- <h1>TEST</h1> -->
-    <p>un petit test</p>
+    <div v-for="estate in estates" :key="estate.id">
+      <h1>{{ estate.name }}</h1>
+      <h2>Prix : {{ estate.price }} euros</h2>
+      <p>Type du bien: {{ estate.type }}</p>
+
+    </div>
+    <p>{{estates}}</p>
 </template>
 
 <script>
 export default {
-  name: 'EstatetView'
+  name: 'EstatetView',
+  data(){
+    return{
+      baseUrl:'http://localhost:3000',
+      estates:[]
+    }
+  },
+  created: async function(){
+    {
+      console.log('test');
+      try{
+            const response = await fetch(`${this.baseUrl}/estate`,
+            {
+                method:'GET',
+                headers:{
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin":"http://localhost:8080"
+                }
+            })
+            const result = await response.json()
+            this.estates = result
+            // this.estates.push(response)
+            console.log(result);
+            
+        } catch(err){
+            console.log(err);
+        }
+    }
+  },
+  methods:{
+    async getEstate(){
+      console.log('test');
+      try{
+            const response = await fetch(`${this.baseUrl}/estate`,
+            {
+                method:'GET',
+                headers:{
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin":"http://localhost:8080"
+                }
+            })
+            const result = await response.json()
+            // this.estates.push(response)
+            console.log(result);
+            
+        } catch(err){
+            console.log(err);
+        }
+    }
+  }
 
   }
 </script>
