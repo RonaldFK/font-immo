b30ef1a8630dcf8d8d7ac13e2685f7fdef301866
Ronald <ronaldfk.pro@gmail.com>
2023-02-20 23:07:51 +0100

    fix multi appel de fonction

--- src/views/EstateDetails.vue
+++ src/views/EstateDetails.vue
@@ -2,7 +2,7 @@
   <router-view></router-view>
   <div class="main-details-div">
     <div class="describe-estate" v-for=" info in oneEstate" :key="info.id">
-      <h1>{{info.name}}</h1>
+      <h2>{{info.name}}</h2>
       <div>
           <p>Prix du bien : {{ info.price }}</p>
           <p>Type de bien : {{ info.type }}</p>
@@ -14,11 +14,13 @@
     <div class="handle-div-describe">
 
       <div class="describe-manager" v-for=" info in oneEstate" :key="info.id">
+        <h2>Manager en charge</h2>
         <p>Prenom : {{ info.manager.firstname }}</p>
         <p>Nom : {{ info.manager.lastname }}</p>
         <p>Mail : {{ info.manager.email }}</p>
       </div>
       <div class="describe-client" v-for=" info in oneEstate" :key="info.id">
+        <h2>Propriétaire</h2>
         <p>Prenom : {{ info.customer.firstname }}</p>
         <p>Nom : {{ info.customer.lastname }}</p>
         <p>Numéro : {{ info.customer.tel }}</p>
@@ -41,10 +43,8 @@
        accept="image/png, image/jpeg">
       <input type="submit" value="Valider" class="btn" @click="addPhoto">
     </form>
-<button @click="getPhoto">test</button>
 </div>
 </div>
-
 </template>
 
 <script>
@@ -59,33 +59,7 @@ export default {
       
     }
   },
-  methods:{
-    addPhoto(e){
-      e.preventDefault()
-      console.log('test');
-    },
-    async getPhoto(){
-    console.log('TESTGET');
-      try{
-            const response = await fetch(`http://localhost:3000/photo`,
-            {
-                method:'GET',
-                headers:{
-                    "Content-Type": "multipart/form-data",
-                    "Access-Control-Allow-Origin":"http://localhost:8080"
-                }
-            })
-            let  result = await response.json()
-            result = result.filter(elem => elem.name != '.DS_Store')
-            console.log(result,'TEST');
-            this.photoUrl = result
-            
-            
-        } catch(err){
-            console.log(err,'TEST ICI');
-        }
-  },
-  async getEstate(){
+  created: async function getEstateDetails(){
     const id = this.$route.params.id
       // console.log(id);
       try{
@@ -107,13 +81,50 @@ export default {
             console.log(err,'TEST ICI');
         }
   },
-  created(){
-    this.getEstate()
-    this.getPhoto()
+  beforeCreate: async function getPhoto(){
+    console.log('TESTGET');
+      try{
+            const response = await fetch(`http://localhost:3000/photo`,
+            {
+                method:'GET',
+                headers:{
+                    "Content-Type": "multipart/form-data",
+                    "Access-Control-Allow-Origin":"http://localhost:8080"
+                }
+            })
+            let  result = await response.json()
+            result = result.filter(elem => elem.name != '.DS_Store')
+            console.log(result,'TEST');
+            this.photoUrl = result
+            
+            
+        } catch(err){
+            console.log(err,'TEST ICI');
+        }
   },
-  beforeMount() {
-   
-},
+  methods:{
+    async addPhoto(){
+      try{
+            const response = await fetch(`http://localhost:3000/photo`,
+            {
+                method:'GET',
+                headers:{
+                    "Content-Type": "multipart/form-data",
+                    "Access-Control-Allow-Origin":"http://localhost:8080"
+                }
+            })
+            let  result = await response.json()
+            result = result.filter(elem => elem.name != '.DS_Store')
+            console.log(result,'TEST');
+            this.photoUrl = result
+            
+            
+        } catch(err){
+            console.log(err,'TEST ICI');
+        }
+  }
+    
+  
   }
 
   }
@@ -123,41 +134,50 @@ export default {
 <style>
 .main-details-div{
   display: flex;
-  margin-top:12rem;
-  margin-left:20rem;
-  margin-right:20rem;
-  background: red;
-  width: 80rem;
-  height: 50rem;
+    margin-top: 12rem;
+    margin-left: 50%;
+    width: 54rem;
+    height: 26rem;
+    background: rgb(194, 222, 209);
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
 }
 .describe-estate{
-  background: black;
-  width: 50%;
+    width: 50%;
+    border-radius: 17px;
+    margin-bottom: 1rem;
+    margin-right: 1rem;
+    height: 25rem;
+    padding-left: 1rem;
+}
+.describe-estate h2{
+  text-align: center;
 }
 .describe-manager{
-  background: blue;
-  height: 50%;
+    padding-left: 1rem;
+}
+.describe-manager h2{
+  text-align: center;
 }
 .describe-client{
-  background:mediumturquoise;
-  height: 50%;
+  padding-left: 1rem;
 }
-.describe-photo{
+.describe-client h2{
+  text-align: center;
+}
+.describe-photo{ 
   display: flex;
-  align-content: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 1rem;
-  margin-left:20rem;
-  margin-right:20rem;
-  background: red;
-  width: 80rem;
-  height: 30rem;
-  position: relative;
-  margin: auto;
-  /*  */
-  /* overflow-y:auto; */
-    /* overflow-x:hidden; */
+    align-content: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1rem;
+    margin-left: 50%;
+    width: 54rem;
+    height: 23rem;
+    position: relative;
+    background: rgb(194, 222, 209);
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
 }
 .describe-photo div{
     display: flex;
@@ -165,18 +185,21 @@ export default {
     margin-left: 1rem;
     margin-right: 1rem;
     overflow-y:auto;
-
+    width: 52rem;
 }
 .describe-photo img{
   border-radius: 5px;
   margin-top: 50px;
   width: 32rem;
-  height: 20rem;
+  height: 15rem;
   vertical-align: middle;
 }
 .handle-div-describe{
-  background: palegreen;
-  width: 50%;
+    width: 50%;
+    border-radius: 17px;
+    margin-bottom: 1rem;
+    margin-right: 1rem;
+    height: 25rem;
 }
 .div-form{
   justify-content: center;
