7037701db49d8344c3ee740f2382bc4a0ded89fe
Ronald <ronaldfk.pro@gmail.com>
2023-03-28 10:57:31 +0200

    fix bug affichage id lors rebuild filtre customers

--- src/components/modales/modalCreateEstate.vue
+++ src/components/modales/modalCreateEstate.vue
@@ -51,9 +51,11 @@
       </v-card>
       <v-card max-width="500" class="w-100 mt-5">
         <v-text-field
+          prepend-icon="mdi-view-dashboard"
+          @click:prepend="searchCustomer(customerToSearch)"
           v-model="customerToSearch"
           ref="customerToSearch"
-          label="Propriétaire :"
+          label="Rechercher propriétaire :"
         ></v-text-field>
         <v-select
           v-model="firstFilterToSearchCustomer"
@@ -66,6 +68,7 @@
         <v-select
           v-model="estateToCreate.customer_id"
           :items="customers"
+          ref="firstname"
           item-title="firstname"
           item-value="id"
           label="prénom"
@@ -158,19 +161,13 @@ export default {
       this.searchCustomer(value);
     },
     firstFilterToSearchCustomer: function (value) {
-      console.log(value);
+      // remise à zéro de l'id prénom qui s'affichait à la regénération du filtre.
+      this.estateToCreate.customer_id = '';
       this.searchCustomer(value);
     },
   },
   created: async function getAllLocation() {
     try {
-      // const response = await fetch(`${this.baseUrl}/location`, {
-      //   method: 'GET',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'Access-Control-Allow-Origin': 'http://localhost:8080',
-      //   },
-      // });
       const managers = await fetch(`${this.baseUrl}/manager`, {
         method: 'GET',
         headers: {
@@ -179,16 +176,16 @@ export default {
         },
       });
       this.managers = await managers.json();
-      console.log(this.managers, 'managers');
     } catch (err) {
-      console.log(err, 'TEST ICI');
+      console.log(err);
     }
   },
   methods: {
     test(value) {
-      return value;
+      console.log(value);
     },
     async searchCustomer(value) {
+      // Déclenchement de la recherche uniquement à partir de 3 caractères
       if (value.length > 2) {
         try {
           const customer = await fetch(
@@ -203,20 +200,18 @@ export default {
           );
           const result = await customer.json();
           result.length > 0 && (this.customers = result);
-          console.log(result);
-          // result[0].id
-          //   ? (this.estateToCreate.location_id = result[0].id)
-          //   : (this.estateToCreate.location_id = null);
         } catch (err) {
-          console.log(err, 'TEST ICI');
+          console.log(err);
         }
       }
     },
     closeModal() {
       this.$router.push('/estate');
     },
+    /**
+     * Création d'un bien ainsi d'une nouvelle localisation
+     */
     async createEstate() {
-      console.log(this.estateToCreate.customer_id);
       const urlLocation = 'http://localhost:3000/location';
       const form = new FormData();
       const urlEstate = 'http://localhost:3000/estate';
@@ -233,11 +228,12 @@ export default {
           body: JSON.stringify(this.locationToCreate),
         });
         const result = await location.json();
+        // gestion d'erreur : si la localisation est bien créé, je passe son id, sinon null
         result[0].id
           ? (this.estateToCreate.location_id = result[0].id)
           : (this.estateToCreate.location_id = null);
       } catch (err) {
-        console.log(err, 'TEST ICI');
+        console.log(err);
       }
       form.append('estate', JSON.stringify(this.estateToCreate));
       try {
@@ -256,69 +252,4 @@ export default {
   },
 };
 </script>
-<style scoped>
-.modal-estate form {
-  display: flex;
-  flex-wrap: wrap;
-  width: 44rem;
-  height: 18rem;
-  margin-top: 10px;
-  margin-bottom: 85px;
-  align-items: center;
-}
-.modal-estate div form input {
-  width: 20rem;
-}
-.modal-estate div form label {
-  width: 20rem;
-}
-.modal-estate div form select {
-  width: 20rem;
-}
-.modal-estate div {
-  width: 54rem;
-  display: flex;
-  flex-direction: column;
-  padding-right: 10rem;
-  padding-left: 10rem;
-  flex-wrap: wrap;
-  align-items: baseline;
-  justify-content: space-between;
-}
-.modal-estate div form > h2 {
-  color: red;
-  text-align: center;
-}
-.modal-estate {
-  /* display: flex; */
-  flex-direction: column;
-  position: relative;
-  margin-left: 20%;
-  margin-right: 20%;
-  margin-top: 8%;
-  /* display: flex; */
-  /* overflow-y: scroll; */
-  /* overflow-x: hidden; */
-  align-items: center;
-  height: 40rem;
-  background: rgba(210, 204, 204, 0.433);
-}
-.modal-estate form div {
-  display: flex;
-  flex-direction: row;
-}
-.button {
-  margin-top: 10px;
-  padding-bottom: 10px;
-  height: 15px;
-}
-.overlay {
-  background: rgba(44, 41, 41, 0.433);
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  z-index: 2;
-}
-</style>
+<style scoped></style>
